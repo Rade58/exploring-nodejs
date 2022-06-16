e19e911085232869825de9788a729336d655a3d8
Rade58 <bajic.rade2@gmail.com>
2022-06-16 17:04:29 +0200

    🦉

--- _exercise/1_streams_intro.js
+++ _exercise/1_streams_intro.js
@@ -1,6 +1,11 @@
 const {createReadStream} = require("fs")
+const through = require("through2")
 
 console.log(process.argv)
 
 
-createReadStream(process.argv[2]).pipe(process.stdout)
+createReadStream(process.argv[2])
+// WE ARE GOING TO TRANSFORM TO UPPERCASE ALL THE DATA OF READABLE STREAM
+.pipe()
+
+.pipe(process.stdout)
