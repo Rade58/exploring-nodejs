518755a31514d825ec98de48a298a2d265bda9b1
Rade58 <bajic.rade2@gmail.com>
2022-06-16 18:15:50 +0200

    🦉

--- _exercise/1_streams_intro.js
+++ _exercise/1_streams_intro.js
@@ -5,8 +5,13 @@ const writeToUppercase = (chunkBuffer, bufferEncoding, next ) => {
   next(null, chunkBuffer.toString().toUpperCase())
 }
 
-
-createReadStream(process.argv[2])
+// INSTEAD OF CREATING NEW READABLE STREAM
+// THT WOULD TAKE CONTENTS OF FILE YOU FEED THE SCRIPT
+// createReadStream(process.argv[2])
+// I AM GOING TO USE STANDARD INPUT (LIKE I MENTIONED TO YOU,
+// HE IS ALSO A READABLE STREAM)
+process.stdin
+// THE REST STAYS THE SAME (WE ARE TRANSFORMING DATA)
 .pipe(through(writeToUppercase))
 .pipe(process.stdout)
 
