9229309c6c57eea9507cb23c41cd6e0a46a114f5
Rade58 <bajic.rade2@gmail.com>
2022-06-16 22:12:55 +0200

    🦉

--- _exercise/concat-stream.js
+++ _exercise/concat-stream.js
@@ -1,9 +1,10 @@
 const concat = require("concat-stream");
+const through = require("through2")
+
 
 // LETS CALCULATE LENGHT IN BYTES FOR STANDARD INPUT
-
 process.stdin.pipe(concat((buf) => {
-  console.log(buf.length)
+  console.log(`LENGTH OF ENTIRE STANDARD INPUT ${buf.length}`)
 }))
 // THIS CAN'T BE PIPED ANYMORE SINCE CONCAT WAS THE WRITABLE STREAM
 
