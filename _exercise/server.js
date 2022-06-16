ec448f3469c59dc6b30f71d360695f9acfa5847b
Rade58 <bajic.rade2@gmail.com>
2022-06-16 23:44:00 +0200

    🦉

--- _exercise/server.js
+++ _exercise/server.js
@@ -1,11 +1,36 @@
 const http = require("http")
+const concat = require("concat-stream")
 
 const server = http.createServer(async (req, res) => {
 
-  console.log(req.rawHeaders);
+  if(req.method !== "POST") {
+    res.statusCode = 404
+    return res.end();
+  }
+  
+  // YOU NEED TO KNOW THIS ONE THING
+  // req    IS ALSO A READABLE STREAM
+  
+  
+  // SO WE CAN USE pipe
 
-  res.statusCode = 201;
-  res.end("Hello World")
+
+  req.pipe(concat((buff) => {
+
+    // IT IS OBVIOUS WHAT WE DID HERE
+    
+    if(buff.length > 26) {
+      res.statusCode = 416
+      res.statusMessage = "Range exceeded"
+      return res.end()
+    }else{
+      res.statusCode = 200
+      res.end("ok")
+    }
+
+  }))
+
+
 
 })
 
