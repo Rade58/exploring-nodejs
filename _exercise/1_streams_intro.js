4615d0db4de85dfa37b458a6113b81d02e894916
Rade58 <bajic.rade2@gmail.com>
2022-06-16 17:17:00 +0200

    🦉

--- _exercise/1_streams_intro.js
+++ _exercise/1_streams_intro.js
@@ -3,9 +3,24 @@ const through = require("through2")
 
 console.log(process.argv)
 
+// THIS IS GOING TO BE TRANSFORM FUNCTION
+// WE WANT TO DEFINE FUNCTION WIH WILL DO UPPERCASE ON
+// A CHARACTER
+
+
+// THIS FUNCTION IS GOING 
+
+const writeToUppercase = (chunkBuffer, bufferEncoding, next ) => {
+ 
+    // FIRST ARGUMENT IS ERROR (WE PASSED null FOR THAT)
+    next(null, chunkBuffer.toString().toUpperCase())
+
+
+}
+
 
 createReadStream(process.argv[2])
 // WE ARE GOING TO TRANSFORM TO UPPERCASE ALL THE DATA OF READABLE STREAM
-.pipe()
+// .pipe()
 
-.pipe(process.stdout)
+// .pipe(process.stdout)
