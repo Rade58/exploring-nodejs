2cb0d1bd81fb2d2c7475115ff4acad217eda9c84
Rade58 <bajic.rade2@gmail.com>
2022-06-17 23:09:35 +0200

    🦉

--- _exercise/foobarz.js
+++ _exercise/foobarz.js
@@ -1,19 +1,18 @@
 const {createReadStream, createWriteStream} = require("fs")
 const {createGunzip, createGzip} = require("zlib")
+const {createHash} = require("crypto")
 
-// LETS CREATE READABLE STREAM FROM A FILE
 const readable = createReadStream("_exercise/my_server.js")
-// LETS DO SOME COMPRESSION
-.pipe(createGzip())
-// LETS WRITE THAT TO A NEW FILE
-.pipe(createWriteStream("_exercise/zlib/my_server.js.gzip"))
+  .pipe(createGzip())
+  .pipe(createWriteStream("_exercise/zlib/my_server.js.gzip"))
 
 
-// WHEN WRITING IS DONE LETS THROW CONTENT
-// OF COMPRESSED FILE TO STANDARD OUTPUT
 
 readable.once("finish", () => {
   createReadStream('_exercise/zlib/my_server.js.gzip')
     .pipe(createGunzip())
+    // HASHING CHUNK
+    .pipe(createHash("sha256", {encoding: "hex"}))
+    // PIPING CHUNK TO STANDARD OUTPUT
     .pipe(process.stdout)
 })
