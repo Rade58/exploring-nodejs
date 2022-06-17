63aed8625c3f9d21ede33545a50089ab85aa5980
Rade58 <bajic.rade2@gmail.com>
2022-06-17 19:26:32 +0200

    🦉

---
+++ _exercise/my_client.js
@@ -0,0 +1,27 @@
+const {request} = require("http");
+
+// MAYBE HERE WE CAN USE STANDARD IN LIKE A
+// WAY TO PRIVIDE DATA FOR THE REQUEST
+
+
+const req = request({
+  host: "localhost",
+  port: 3006,
+  url: "/",
+  method: "POST",
+
+
+}, (res) => {
+
+})
+
+process.stdin.on("end", () => {
+  
+  req.end()
+
+})
+
+process.stdin.pipe(req)
+
+
+
