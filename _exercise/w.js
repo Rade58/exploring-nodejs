8e8a8e10e312871e759fd392e2b0c0a70d82e04c
Rade58 <bajic.rade2@gmail.com>
2022-06-17 15:36:50 +0200

    🦉

--- _exercise/w.js
+++ _exercise/w.js
@@ -3,21 +3,24 @@ const {createWriteStream} = require('fs');
 // WE WILL CREATE WRITABLE STREAM FROM CONTENTS OF THE FILE
 // "_exercise/someFile.txt"
 
-const w = createWriteStream("_exercise/someFile.txt")
 
-w.on("finish", (a) => {
-  console.log({a})
+const w1 = createWriteStream("_exercise/someFile1.txt")
+const w2 = createWriteStream("_exercise/someFile2.txt")
+
+// THIS DOESN'T WORK
+// I TRIED, BUT EVENT DOESN'T TRIGGER AT ALL
+w1.on("finish", (a) => {
+  console.log("Hello World")
 })
 
  
 // KNOW THAT PREVIOUS DATA WILL BE ERASED 
-w.write("Hello World \n")
-w.write("Hello World \n")
-w.write("Hello World \n")
-w.write("Hello World \n")
-w.write("Hello World \n")
-w.write("Hello World \n")
-w.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
+w1.write("Hello World \n")
 
-
-
+w2.write("Hello Bioconstructor\n")
