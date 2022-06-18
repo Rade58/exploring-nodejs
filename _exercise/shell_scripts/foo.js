b254bb2f4a59b4a9648d263fa663b2c983f1d98f
Rade58 <bajic.rade2@gmail.com>
2022-06-18 23:20:29 +0200

    🦉

--- _exercise/shell_scripts/foo.js
+++ _exercise/shell_scripts/foo.js
@@ -1,24 +1,57 @@
 #!/usr/bin/env node
 "use strict"
 
+const fs = require('fs');
+const path = require("path")
+
+
 const minimist = require("minimist");
 
 const argv = minimist(process.argv, {
   boolean: ["help"],
-  string: ["file"],
-  // I ADDED THIS
-  default: {
-    file: "/_exercise/bazbaz.js",
-  }
+  string: ["file"],  
 })
+
+console.log(argv["file"])
 
 console.log(argv)
 
+if(argv["help"]){
+  printHelp()
+}else if(argv["file"]){
+  // WE READ THE FILE
+
+  const absPath = path.resolve(argv["file"])
+
+  const fileContent = fs.readFileSync(absPath)
+  
+  // console.log(fileContent)
+  console.log(absPath)
+
+  process.stdout.write(fileContent)
+
+}else{
+  error("Wrong stuff", true)
+}
+
+
+
+
+
 // *************************
-function getHelp(){
+function printHelp(){
   console.log(`
     foo.js --help
 
   --help          try getting help
   `)
+}
+
+function error(msg, includeHelp = false){
+
+  console.error(msg)
+
+  if(includeHelp){
+    printHelp()
+  }
 }
