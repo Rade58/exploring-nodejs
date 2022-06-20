7b02aa34042f5fdb64a5ae439bd3ed6244734106
Rade58 <bajic.rade2@gmail.com>
2022-06-21 00:35:53 +0200

    🦉

--- _exercise_2/foobar.js
+++ _exercise_2/foobar.js
@@ -5,18 +5,13 @@ const {createReadStream, createWriteStream} = require("fs")
 const path = require("path")
 const {Transform} = require("stream")
 
-// IT IS AN ANOTHER USEFUL NATIVE PACKAGE
-const {createGzip} = require("zlib")
+// WE NEED createGunzip
+const {createGzip, createGunzip} = require("zlib")
 // 
 
 const minimist = require("minimist");
 
-
-
 const args = minimist(process.argv.slice(2), {
-  // ADDING ANOTHER BOOLEAN
-  // WE WANT TO BE ABLE TO USE --compress FLAG
-  // WHEN RUNNING THE SCRIPT
   boolean: ["help", "compress"],
   string: ["file"]
 })
@@ -34,9 +29,6 @@ if(!args["file"]){
 const filePath = path.resolve(args["file"])
 
 
-// SO IF A USER DID WANT TO COMPRESS WE WANT TO USE ANOTHER
-// WRITE STREAM WITH .gzip EXTENSION
-// THAT IS WHY WE ARE USING TERNARY
 const myWriteStream = createWriteStream(
   "_exercise_2/some_file.txt" + ( args["compress"] ? ".gzip":"")
 )
@@ -65,27 +57,33 @@ neuReadStream.on("error", (err) => {
   printError(err.message)
 })
 
-// WE NEED TO PIPE TO COMPRESSION STREAM
-// CONDITIONALLY
-// SO INSTEAD OF THIS WHICH WE HAD EARLIER
-// neuReadStream
-  // .pipe(transToUpperCase)
-  // .pipe(myWriteStream);
 
-// WE DECLARE VARIABLE 
-const toUpperCaseStream = neuReadStream
-  .pipe(transToUpperCase)
-
-// AND THE REST OF PIPING IS CONDITIONAL
 
 let ourNewStream;
 
-if(args["compress"]){
+const toUpperCaseStream = neuReadStream
+.pipe(transToUpperCase)
 
-  ourNewStream = toUpperCaseStream.pipe(createGzip())
 
+// LETS ADD IF STATEMENT OVER HERE
+if(filePath.endsWith("gzip")){
+
+  // AND WE DO THIS
+  ourNewStream = neuReadStream.pipe(createGunzip())
+    
 }else{
-  ourNewStream = toUpperCaseStream
+
+  // WE DON'T NEED TO CHANGE ANYTHING IN HERE 
+  
+  
+  
+  if(args["compress"]){
+    
+    ourNewStream = toUpperCaseStream.pipe(createGzip())
+    
+  }else{
+    ourNewStream = toUpperCaseStream
+  }
 }
 
 // WE THEN CAN PIPE TO FILE
