54d7ca30ea3362e5e18b22a50f73051be325394d
Rade58 <bajic.rade2@gmail.com>
2022-06-20 23:01:14 +0200

    🦉

--- _exercise_2/foobar.js
+++ _exercise_2/foobar.js
@@ -1,18 +1,46 @@
 #!/usr/bin/env node
 "use strict"
 
-// WE NEED ALSO A createWriteStream
 const {createReadStream, createWriteStream} = require("fs")
 const path = require("path")
-
 const {Transform} = require("stream")
+
+// IT IS AN ANOTHER USEFUL NATIVE PACKAGE
+const {createGzip} = require("zlib")
+// 
 
 const minimist = require("minimist");
 
 
-const myWriteStream = createWriteStream("_exercise_2/some_file.txt")
 
-// ADDING AN EVENT
+const args = minimist(process.argv.slice(2), {
+  // ADDING ANOTHER BOOLEAN
+  // WE WANT TO BE ABLE TO USE --compress FLAG
+  // WHEN RUNNING THE SCRIPT
+  boolean: ["help", "compress"],
+  string: ["file"]
+})
+
+
+if(args["help"]){
+  return printHelp()
+}
+
+if(!args["file"]){
+  return printError("You must provide file path", true)
+}
+
+
+const filePath = path.resolve(args["file"])
+
+
+// SO IF A USER DID WANT TO COMPRESS WE WANT TO USE ANOTHER
+// WRITE STREAM WITH .gzip EXTENSION
+// THAT IS WHY WE ARE USING TERNARY
+const myWriteStream = createWriteStream(
+  "_exercise_2/some_file.txt" + args["compress"] ? ".gzip":""
+)
+
 myWriteStream.once("finish", () => {
   process.stdout.write("You wrote to file\n")
 })
@@ -30,25 +58,6 @@ const transToUpperCase = new Transform({
   }
 })
 
-
-const args = minimist(process.argv.slice(2), {
-  boolean: ["help"],
-  string: ["file"]
-})
-
-
-if(args["help"]){
-  return printHelp()
-}
-
-if(!args["file"]){
-  return printError("You must provide file path", true)
-}
-
-
-const filePath = path.resolve(args["file"])
-
-
 const neuReadStream = createReadStream(filePath)
 
 neuReadStream.on("error", (err) => {
@@ -56,8 +65,11 @@ neuReadStream.on("error", (err) => {
   printError(err.message)
 })
 
+// WE DON'T NEED TO CHANGE ANYTHING IN HERE
+neuReadStream
+  .pipe(transToUpperCase)
+  .pipe(myWriteStream);
 
-neuReadStream.pipe(transToUpperCase).pipe(myWriteStream);
 
 // *****************************************
 
