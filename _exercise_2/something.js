864dec808cc786443d03cb6ba9519dcfcf1490da
Rade58 <bajic.rade2@gmail.com>
2022-06-21 18:18:36 +0200

    🦉

--- _exercise_2/something.js
+++ _exercise_2/something.js
@@ -2,19 +2,55 @@ const {
   createReadStream,
   createWriteStream} = require("fs");
 
-// LETS CREATE READ STREAM
 
-console.log("random things")
+// WE DEFINE THIS FUNCTION
+const pipeAsync = async (readable, writable) => {
+
+  return new Promise((res, rej) => {
+    
+    // RESOLVE WHEN WRITING TO STREAM ENDED
+
+    writable.once("finish", () => {
+      res({readable, writable})
+    })
+
+    writable.on("error", (err) => {
+      rej(err)
+    })    
+
+    // WE CAN START PIPING HERE
+    readable.pipe(writable)
+    console.log("piping started")
+
+  })
+
+}
+
+// LETS TEST IT
 
 const readStream = createReadStream("_exercise_2/blog-post.mdx");
+const writeStream = createWriteStream("_exercise_2/another-blog-post.mdx")
 
-console.log("random things 2")
+async function main() {
 
-readStream.on("end", () => {
-  console.log("reding ended")
-})
+  // THIS SHOULD PRINT "BEFORE PIPING" START
+  console.log("before piping")
+
+  try{
+
+    const streams = await pipeAsync(readStream, writeStream)
 
 
+  }catch(err){
+
+    console.error(err)
+
+  }
+
+  // THIS SHOULD BE PRINTED AFTER PIPING HAPPENS
+  console.log("after piping")
+
+}
 
 
-
+main()
