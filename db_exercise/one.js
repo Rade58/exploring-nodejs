5974898948ed408ae15240a6816febeaf4c622cf
Rade58 <bajic.rade2@gmail.com>
2022-06-23 19:36:23 +0200

    🦉

--- db_exercise/one.js
+++ db_exercise/one.js
@@ -1,9 +1,40 @@
 const path = require("path")
+const {readFileSync} = require("fs")
 const {promisify} = require("util");
+const {Database} = require("sqlite3")
 
 const DB_PATH = path.join(__dirname,"my.db");
 
-const DB_SQL_PATH = path.resolve(__dirname, "mydb.sql")
+const DB_SQL_PATH = path.resolve(__dirname, "../mydb.sql");
 
-console.log(DB_SQL_PATH)
+const initSql = readFileSync(DB_SQL_PATH, "utf-8");
+
+// THIS IS GOING TO BE OUR HELPERS, LIKE I TOLD YOU
+let SQL3; // (WE WILL DECLARE THEM ON THIS OBJEECT)
+
+
+async function main(){
+  
+  const myDB = new Database(DB_PATH);
+
+  SQL3 = {
+    run(...args){
+      return new Promise((res, rej) => {
+        myDB.run(...args, function (err) {
+          if(err) rej(err);
+          else res(this)
+        })
+      })
+    },
+    get: promisify(myDB.get.bind(myDB)),
+    all: promisify(myDB.all.bind(myDB))
+  }
+
+
+
+
+}
+
+
+
 
