42b9ca906766100bd925fbe3ac5d9eb369df8048
Rade58 <bajic.rade2@gmail.com>
2022-06-23 18:28:21 +0200

    🦉

--- db_exercise/one.js
+++ db_exercise/one.js
@@ -1,1 +1,9 @@
-console.log(__dirname)
+const path = require("path")
+const {promisify} = require("util");
+
+const DB_PATH = path.join(__dirname,"my.db");
+
+const DB_SQL_PATH = path.resolve(__dirname, "mydb.sql")
+
+console.log(DB_SQL_PATH)
+
