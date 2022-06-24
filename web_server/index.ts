b7c38955d6788f4195f0a8cf1517b763a59d7043
Rade58 <bajic.rade2@gmail.com>
2022-06-24 15:31:59 +0200

    🦉

--- web_server/index.ts
+++ web_server/index.ts
@@ -10,7 +10,6 @@ const HTTP_PORT = 8066;
 async function initServer() {
   
   const client = await initDB()
-
   
   const httpServer = createServer(handler)
   
@@ -20,13 +19,22 @@ async function initServer() {
   
   async function handler(req: IncomingMessage, res: ServerResponse){
 
-    console.log({client})
+    // WE ARE USING req.url
+    // SERVE DATA IF PATH IS "/hello"
 
-    res.writeHead(200, {
-      "Content-Type": "text/plain"
-    })
-    res.write("Hello world")
-    res.end()
+    if(req.url === "/hello"){
+      res.writeHead(200, {
+        "Content-Type": "text/plain"
+      })
+      res.write("Hello world")
+      res.end()
+    }else{
+      // IF NOT SEND 404
+      res.writeHead(404)
+      res.end()
+    }
+   
+
   }
 
 
