98a719fb4335b576f40188708ff7432966e0ac7e
Rade58 <bajic.rade2@gmail.com>
2022-06-24 18:31:43 +0200

    🦉

--- web_server/file-server.ts
+++ web_server/file-server.ts
@@ -3,10 +3,26 @@ import {Server} from 'node-static-alias'
 import path from 'path'
 
 // THIS PATH IS GOING TO BE THE DOCUMENT ROOTH
+// FOR US IT IS       web_server/web_files
+// BUT WE NEED ABSOLUTE PATH SO WE ARE USING THIS
 const WEB_FILES_PATH = path.resolve(__dirname, "web_files");
+// BASICALY WE ARE GOING TO MAKE web_server/web_files
+// AND PUT OUR HTML FILES THERE
 
-console.log({WEB_FILES_PATH})
+// console.log({WEB_FILES_PATH})
 
+// THIS PACKAGE BASICALY USES node-static UNDER THE HOOD
+// BUT IT HAS SUPPORT FOR ALIASES
+
+// BASICALLY WE WILL HAVE A REGEX MATCHING
+// IF USER REQUESTS    /         index.html IS GOING TO BE SERVED
+// OFCOURSE WE NEED TO DEFINE THOSE REGEX
+// BUT I AM NOT PLANING TO WRITE THEM BY MYSELF
+// I'M GOING COPY THEM
+
+// YOU WILL REMEMBER THIS KIND OF SYNTAX
+// YOU WERE DOING THIS WHEN SETTING WEB SERVER LIKE NGINX
+// OR WHEN WE WERE DEALING WITH KUBERNETES
 
 const createFileServer = () => {
 
@@ -14,6 +30,9 @@ const createFileServer = () => {
     cache: 100,
     serverInfo: "Shibatoshi Workshop",
     alias: [
+      // THIS IS FOR MAIN PAGE 
+
+
       {
         match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
         serve: "index.html",
