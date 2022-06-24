1c7b3536f8f25cb56749999fda4c25d5797f5332
Rade58 <bajic.rade2@gmail.com>
2022-06-24 13:50:41 +0200

    🦉

--- web_server/index.ts
+++ web_server/index.ts
@@ -1,4 +1,40 @@
+import {createServer} from 'http'
+import type {IncomingMessage, ServerResponse} from 'http'
+
 import initDB from './db'
 
+const HTTP_PORT = 8066;
 
-initDB()
+
+
+async function initServer() {
+  
+  const client = await initDB()
+
+  
+  const httpServer = createServer(handler)
+  
+  httpServer.listen(HTTP_PORT, () => {
+    console.log(`Server on poort ${HTTP_PORT}`)
+  })
+  
+  // THIS IS GOING TO BE OUR HANDLER
+  // async function handler()
+  async function handler(req: IncomingMessage, res: ServerResponse){
+
+    // LETS JUST WRITE SOMETHING TO TST IT
+    // AND DON'T FORGET THAT WE ARE DEALING WITH STREAMS
+
+    console.log({client})
+
+    res.statusCode = 200
+    res.write("Hello world")
+    res.end()
+  }
+
+
+
+
+}
+
+
