e4670347e48c9cd7b848119bbc2e9cfeea5eb696
Rade58 <bajic.rade2@gmail.com>
2022-06-24 21:12:01 +0200

    🦉

--- web_server/db.ts
+++ web_server/db.ts
@@ -13,13 +13,15 @@ async function initDB(){
   const myDB = new Database(DB_PATH);
 
   const SQL3 = {
-    run(sql: string){
+    run(...args: any){
       return new Promise((res, rej) => {
-        myDB.run(sql, function (err) {
+        // @ts-ignore
+        myDB.run(...args, function (err) {
           if(err){
 
             return rej(err);
           } 
+          // @ts-ignore
           return res(this);
         })
       })
@@ -33,84 +35,70 @@ async function initDB(){
 
   }
 
-
-
   // DEFINED SOME HELPERS HERE ALSO
 
-  const getBonkByInfo = async (info: string) => {
-    
-    const result = await SQL3.get(/* sql */`
-      SELECT * FROM Bonk WHERE info = ?
-    `,
-    // @ts-ignore
-    info)
-
-    return result
-  }
-
-
-  const insertBonk = async (info: string) => {
-
-    return SQL3.run(/* sql */`
-      INSERT INTO Bonk (info) VALUES (?) 
-    `,
-    // @ts-ignore
-    info
-    )
-
-  }
-
-  const getShibaByBonkInfo = async (bonkInfo: string) => {
-    
-    return SQL3.get(/* sql */`
-      SELECT * FROM Shiba
-      LEFT JOIN Bonk 
-      ON Shiba.bonkId = Bonk.id
-      WHERE Bonk.info = ?
-    `,
-    // @ts-ignore
-    bonkInfo)
-
-  }
-
-  const insertShiba = async (info: string, bonkId: number) => {
-
-    return SQL3.run(/* sql */`
-      INSERT INTO Shiba (info, bonkId) VALUES (?, ?)
-    `
-    // @ts-ignore
-    , info, bonkId)
-  }
-
-  const getAllRecords = async () => {
-    return SQL3.all(/* sql */`
-      SELECT
-        Shiba.info AS 'shiba',
-        Bonk.info AS 'bonk'
-      FROM
-        Shiba JOIN Bonk
-        ON (Shiba.bonkId = Bonk.id)
-      ORDER BY
-        Bonk.id DESC, Shiba.info ASC
-    `)
-  }
-
+ 
   // APPEND THESE METHODS ON THE OBJECT
   // WE ARE GOING TO USE AS CLIENT
 
-   const client = {
+  const client = {
       ...SQL3, 
-      getAllRecords,
-      insertShiba, 
-      getShibaByBonkInfo,
-      insertBonk,
-      getBonkByInfo
+      getBonkByInfo: async (info: string) => {
+    
+        const result = await SQL3.get(/* sql */`
+          SELECT * FROM Bonk WHERE info = ?
+        `,
+        // @ts-ignore
+        info)
+    
+        return result
+      },
+      insertBonk: async (info: string) => {
+    
+        return SQL3.run(/* sql */`
+          INSERT INTO Bonk (info) VALUES (?) 
+        `,
+        // @ts-ignore
+        info
+        )
+      },
+      getShibaByBonkInfo: async (bonkInfo: string) => {
+        
+        return SQL3.get(/* sql */`
+          SELECT * FROM Shiba
+          LEFT JOIN Bonk 
+          ON Shiba.bonkId = Bonk.id
+          WHERE Bonk.info = ?
+        `,
+        // @ts-ignore
+        bonkInfo)
+      },
+      insertShiba: (info: string, bonkId: number) => {
+    
+        return SQL3.run(/* sql */`
+          INSERT INTO Shiba (info, bonkId) VALUES (?, ?)
+        `
+        // @ts-ignore
+        , info, bonkId)
+      },
+      getAllRecords: async () => {
+        return SQL3.all(/* sql */`
+          SELECT
+            Shiba.info AS 'shiba',
+            Bonk.info AS 'bonk'
+          FROM
+            Shiba JOIN Bonk
+            ON (Shiba.bonkId = Bonk.id)
+          ORDER BY
+            Bonk.id DESC, Shiba.info ASC
+        `)
+      }
+    
     }
 
-
   // TIS WS HERE BEFORE, WE ARE RUNNING INITIL SQL
-  await client.exec(initSql)
-
+  // 
+  await SQL3.exec(initSql)
   
   console.log("database started");
 
--- web_server/index.ts
+++ web_server/index.ts
@@ -3,6 +3,10 @@ import type {IncomingMessage, ServerResponse} from 'http'
 import initDB from './db'
 import createFileServer from './file-server'
 
+// WE IMPORT SEEDING FUNCTION
+import seed from './seed'
+
+
 const HTTP_PORT = 8066;
 
 const fileServer = createFileServer()
@@ -10,6 +14,16 @@ const fileServer = createFileServer()
 async function initServer() {
   
   const client = await initDB()
+
+  // WE USE SEEDING HERE
+  await seed(client)
+
+  // TO CHECK IF SEEDING REALLY HAPPENED LETS
+  // PRINT ALL RECORDS
+  // WE WILL REMOVE THIS LATER OFCOURSE
+  const table = await client.getAllRecords()
+  console.table(table
+    )
 
   const httpServer = createServer(handler)
   
