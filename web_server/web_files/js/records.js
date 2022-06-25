f1142bfc2154faa46eaf5a2b22286734fcc638f3
Rade58 <bajic.rade2@gmail.com>
2022-06-25 17:41:36 +0200

    🦉

--- web_server/web_files/js/records.js
+++ web_server/web_files/js/records.js
@@ -1,6 +1,10 @@
 (function(){
   
-  // THIS IS OUR FUNCTION 
+
+
+  renderRecords()
+
+  
   async function fetchAllRecords (){
     
     try{
@@ -9,19 +13,58 @@
 
       const data = await resp.json()
 
-      renderRecords(data)
+      return data;
 
     }catch(err){
 
       console.error(err.message)
-        
+      
+      return {records: []}
     }
   }
 
+  // THIS IS HELPER WE ARE USING
+  // AS YOU CAN SEE I AM USING PACKAGE FROM CDN CALLED
+  // json2html
+  function renderData(records, el) {
+    var transforms = {
+            "row": {
+                    "<>": "tr",
+                    html: "<td>${something}</td><td>${other}</td>",
+            },
+            "table": {
+                    "<>": "table",
+                    border: "1",
+                    cellPadding: "10",
+                    html: function table(){
+                            return `<tr><td>Something</td><td>Other</td></tr>
+                                    ${json2html.transform(records,transforms.row)}
+                            `;
+                    },
+            },
+    };
 
-  function renderRecords(data){
-    // 
-    // 
+    el.innerHTML = json2html.transform({},transforms.table);
+  }
+
+
+  function renderRecords(){
+  
+    // WE CAN NOW DEFINE RENDERING
+    const button = document.querySelector("button[rel*=js-button]");
+    const container = document.querySelector("div[rel*=js-records]");
+    
+    const handleClick = () => {
+
+      fetchAllRecords().then(({records}) => {
+        // USING HELPER TO RENDER DATA
+        renderData(records, container)
+      })
+
+    }
+
+    button.addEventListener("click", handleClick, false)
+    
   }
 
 
