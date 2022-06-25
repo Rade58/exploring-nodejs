4cc6ef4edc7fb22d79bd6eabed25e6eb21edf598
Rade58 <bajic.rade2@gmail.com>
2022-06-25 02:11:05 +0200

    🦉

--- web_server/web_files/js/records.js
+++ web_server/web_files/js/records.js
@@ -1,22 +1,8 @@
-
-window.alert("Hello World!")
-
-// console.log("stuff")
-
 (function(){
   
   // LEETS JUST PRINT HELLO WORLD TO TEST THINGS OUT
   
-  window.console.log("hello records")
-
-  alert("something")
-
-  document.querySelector('div[rel*=js-cont').innerHTML = `
-    <div>U Skripcu</div>
-  
-  `
-
-
+  console.log("hello records")
 
 
 })()
