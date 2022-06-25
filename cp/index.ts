264b08606b5b2c210d3d9d7e237bd140a3e87854
Rade58 <bajic.rade2@gmail.com>
2022-06-26 01:45:31 +0200

    🦉

--- cp/index.ts
+++ cp/index.ts
@@ -4,16 +4,18 @@
 import childProc from 'child_process'
 import {promisify} from 'util'
 
-// LETS SPAWN OUR FIRST EVER CHILD PROCESS
 
 main()
 
 
 function main(){
-
+  
+  // LETS SPAWN OUR FIRST EVER CHILD PROCESS
   const child = childProc.spawn("ts-node", ["cp/child1.ts"])
 
   child.on("exit", (code, signal) => {
+    // THIS IS GOING TO BE PRINTED WHEN 
+    // CHILD PROCESS FINISHES ITS OWN RUN
     console.log({code, signal})
   } )
 
