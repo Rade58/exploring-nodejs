3f0747ae4b73fdb630f5cdaf73906f5df3a81e41
Rade58 <bajic.rade2@gmail.com>
2022-06-26 02:07:56 +0200

    🦉

--- cp/index.ts
+++ cp/index.ts
@@ -10,12 +10,13 @@ main()
 
 function main(){
   
-  // LETS SPAWN OUR FIRST EVER CHILD PROCESS
-  const child = childProc.spawn("ts-node", ["cp/child1.ts"])
+
+  const child = childProc.spawn(
+    "ts-node", 
+    ["cp/child1.ts"]
+  )
 
   child.on("exit", (code, signal) => {
-    // THIS IS GOING TO BE PRINTED WHEN 
-    // CHILD PROCESS FINISHES ITS OWN RUN
     console.log({code, signal})
   } )
 
